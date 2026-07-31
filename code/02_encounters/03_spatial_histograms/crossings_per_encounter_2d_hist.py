430db09c3232b80fee4fec482ffd047732dee1cb
archpc <ocarrolloisin@gmail.com>
2026-07-31 12:17:30 +0100

    Encounter spatial histograms

---
+++ code/02_encounters/03_spatial_histograms/crossings_per_encounter_2d_hist.py
@@ -0,0 +1,197 @@
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib import colors
+from matplotlib.colors import LogNorm
+
+import numpy as np
+
+import os
+
+from astropy.table import QTable, vstack
+from astropy.time import Time
+import astropy.units as u
+
+from hermpy.utils.constants import Constants
+from hermpy.net.client_spice import ClientSPICE
+
+from hermpymod.classes.panels import PlanarplotPanel, HistogramPanel
+from hermpymod.functions.ephemeris_downsampler import parse_crossing_list, abs_r, parse_spice, time_array
+from hermpymod.functions.encounters import encounter_finder, parse_encounters_list
+from hermpymod.functions.data_per_orbit import orbit_data
+from hermpymod.functions.ephemeris_downsampler import parse_periapsis_data
+from hermpymod.functions.boundary_models_mod import plot_magnetospheric_boundaries
+from hermpymod.functions.plot_all import plot_all_ephemeris
+from hermpymod.functions.downsampled_positional_data import parse_spice_downsampled
+
+home_dir = os.getenv('HOME')
+data_dir = os.path.join(home_dir, '.ephemeris_data/')
+img_dir = "../../../plots_and_images/"
+
+
+def arclength_midpoint(t, x, y, z=None):
+    """
+    Find the midpoint (by arc length) of a curve given arrays of coordinates.
+    x, y, (z) can be in any consistent units (e.g. km, or Mercury radii).
+    Returns the index of the point closest to the midpoint, the interpolated
+    midpoint coordinates, and the cumulative arc length array.
+    """
+    coords = np.column_stack([x, y] if z is None else [x, y, z])
+    
+    # distance between consecutive points
+    diffs = np.diff(coords, axis=0)
+    seg_lengths = np.linalg.norm(diffs, axis=1)
+    
+    # cumulative arc length, starting at 0
+    cumlen = np.concatenate([[0], np.cumsum(seg_lengths)])
+    total_length = cumlen[-1]
+    half_length = total_length / 2
+    
+    # index of last point before the midpoint
+    idx = np.searchsorted(cumlen, half_length) - 1
+    idx = np.clip(idx, 0, len(coords) - 2)
+    
+    # linear interpolation between coords[idx] and coords[idx+1]
+    seg_frac = (half_length - cumlen[idx]) / seg_lengths[idx]
+    midpoint = coords[idx] + seg_frac * diffs[idx]
+    time = t[idx] + (t[idx+1] - t[idx])/2
+    
+    return idx, midpoint, time, cumlen
+
+
+Zd = Constants.DIPOLE_OFFSET.to("Mercury Radii")
+
+plt.style.use(img_dir + "presentation.mplstyle")
+
+crossing_data = parse_crossing_list()
+
+bins = np.arange(0,7, 1)
+
+hollman_encounters_data = encounter_finder(crossing_data)
+ephemeris_data = parse_spice_downsampled()
+ephemeris_data["UTC"] = ephemeris_data["UTC"].to_datetime()
+
+ephemeris_data["Rho MSM"] = np.sqrt(ephemeris_data["Y MSM"]**2 + ephemeris_data["Z MSM"]**2)
+
+average_encounter = []
+
+encounter_list = parse_encounters_list()
+encounter_times_start = Time(encounter_list["Time Start"]).to_datetime()
+encounter_times_end = Time(encounter_list["Time End"]).to_datetime()
+
+encounter_index_start = np.searchsorted(ephemeris_data["UTC"], encounter_times_start)
+encounter_index_end = np.searchsorted(ephemeris_data["UTC"], encounter_times_end, "right")
+
+encounter_indices = list(zip(encounter_index_start.tolist(), encounter_index_end.tolist()))
+
+encounter_indices = np.array(encounter_indices)
+
+crossings_per_encounter = [len(i) for i in hollman_encounters_data]
+crossings_per_encounter = np.array(crossings_per_encounter)
+
+bs_mask = ["BS" in item for item in encounter_list["Label"]]
+mp_mask = ["MP" in item for item in encounter_list["Label"]]
+
+type_config = [
+        ("Bow Shock", bs_mask),
+        ("Magnetopause", mp_mask),
+        ]
+
+
+for title, mask in type_config:
+
+    start_idx, end_idx = encounter_indices[mask].T
+
+    fig = plt.figure()
+
+    gs = GridSpec(2, 3, figure=fig)
+
+    ax = [
+            fig.add_subplot(gs[0, 0]),
+            fig.add_subplot(gs[0, 1]),
+            fig.add_subplot(gs[0, 2]), 
+            fig.add_subplot(gs[1, :]),
+            ]
+
+
+    plot_configs = [
+            ("xy", ephemeris_data["X MSM"], ephemeris_data["Y MSM"], ax[0], "X MSM", "Y MSM"),
+            ("xz", ephemeris_data["X MSM"], ephemeris_data["Z MSM"], ax[1], "X MSM", "Z MSM"),
+            ("yz", ephemeris_data["Y MSM"], ephemeris_data["Z MSM"], ax[2], "Y MSM", "Z MSM"),
+            ("Cylindrical", ephemeris_data["X MSM"], ephemeris_data["Rho MSM"], ax[3], "X MSM", r"$\rho$ MSM"),
+            ]
+
+    crossings_per_type = crossings_per_encounter[mask]
+
+    for label, xaxis, yaxis, axis, xlab, ylab in plot_configs:
+
+
+        if label != "Cylindrical":
+            xedges, yedges = np.arange(-7, 7, 0.1), np.arange(-7, 7, 0.1)
+        else:
+            xedges, yedges = np.arange(-7, 7, 0.1), np.arange(0, 10, 0.1)
+
+        nx, ny = len(xedges) - 1, len(yedges) - 1
+
+        
+        number_of_crossings = np.empty((nx,ny), dtype=object)
+
+        # Create bin matrix that is an array of empty lists
+        for i in range(nx):
+            for j in range(ny):
+                number_of_crossings[i,j] = []
+
+        for idx, (s, e) in enumerate(zip(start_idx, end_idx)):
+
+            if s==e:
+                ix = np.digitize(xaxis[s].value, xedges) - 1
+                iy = np.digitize(yaxis[s].value, yedges) - 1
+
+            else:
+                ix = np.digitize(xaxis[s:e].value, xedges) - 1
+                iy = np.digitize(yaxis[s:e].value, yedges) - 1
+
+
+            # Find coordinates of bins that the traversal passed through
+            bin_pairs = np.column_stack([ix, iy])
+            unique_bins = np.unique(bin_pairs, axis=0)
+            
+            # Add traversal duration to bins in which were passed through
+            for bin in number_of_crossings[unique_bins[:, 0], unique_bins[:, 1]]:
+                bin.append(crossings_per_type[idx])
+
+        
+        # For non-empty bins, take bin weighting to be the average duration of the magnetosheath traversal
+        for i in range(nx):
+            for j in range(ny):
+                if number_of_crossings[i,j] == []:
+                    number_of_crossings[i,j] = np.nan
+                else:
+                    number_of_crossings[i,j] = np.average(number_of_crossings[i,j])
+
+
+        number_of_crossings = number_of_crossings.astype(float)
+
+        sc = axis.pcolormesh(xedges, yedges, number_of_crossings.T, norm=LogNorm(), cmap='viridis')
+
+        
+        if label == "Cylindrical":
+            plot_magnetospheric_boundaries(axis, frame='MSM', sub_solar_magnetopause=1.4, cylindrical=True, add_legend=True, zorder=5)
+            axis.set_xlim(-8,6)
+            axis.set_ylim(0,10)
+            axis.legend()
+        else:
+            plot_magnetospheric_boundaries(axis, frame='MSM', sub_solar_magnetopause=1.4, plane=label, add_legend=False, zorder=5)
+            axis.set_xlim(-8,8)
+            axis.set_ylim(-8,8)
+
+        axis.set_xlabel(xlab + " Mercury Radii")
+        axis.set_ylabel(ylab + " Mercury Radii")
+
+
+        if label =="xy":
+            fig.colorbar(sc, ax=ax, label=r'Number of crossings')
+
+
+    fig.suptitle(title + " Encounters in MESSENGER Mission")
+
+plt.show()
---
+++ code/02_encounters/03_spatial_histograms/encounter_per_hour.py
@@ -0,0 +1,174 @@
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib import colors
+from matplotlib.colors import LogNorm
+
+import numpy as np
+
+import os
+
+from astropy.table import QTable, vstack
+from astropy.time import Time
+import astropy.units as u
+
+from hermpy.utils.constants import Constants
+from hermpy.net.client_spice import ClientSPICE
+
+from hermpymod.classes.panels import PlanarplotPanel, HistogramPanel
+from hermpymod.functions.ephemeris_downsampler import parse_crossing_list, abs_r, parse_spice, time_array,  parse_periapsis_data
+from hermpymod.functions.downsampled_positional_data import parse_spice_downsampled
+from hermpymod.functions.encounters import encounter_finder, parse_encounters_list
+from hermpymod.functions.data_per_orbit import orbit_data
+from hermpymod.functions.boundary_models_mod import plot_magnetospheric_boundaries
+from hermpymod.functions.plot_all import plot_all_ephemeris
+
+
+home_dir = os.getenv('HOME')
+data_dir = os.path.join(home_dir, '.ephemeris_data/')
+img_dir = "../../../plots_and_images/"
+
+
+Zd = Constants.DIPOLE_OFFSET.to("Mercury Radii")
+
+plt.style.use(img_dir + "presentation.mplstyle")
+
+crossing_data = parse_crossing_list()
+
+ephemeris_data = parse_spice_downsampled()
+
+ephemeris_data["UTC"] = ephemeris_data["UTC"].to_datetime()
+
+residence_time = [(ephemeris_data["UTC"][i+1] - ephemeris_data["UTC"][i]).total_seconds()/3600 for i in range(len(ephemeris_data) - 1)]
+
+# Make residence time same shape as ephemeris_data
+residence_time.append(0)
+
+ephemeris_rho = np.sqrt(ephemeris_data["Y MSM"]**2 + ephemeris_data["Z MSM"]**2)
+
+hollman_encounters_data = parse_encounters_list()
+
+encounter_bs = vstack([i for i in hollman_encounters_data if "BS" in i["Label"]])
+encounter_mp = vstack([i for i in hollman_encounters_data if "MP" in i["Label"]])
+
+bs_encounter_start_time = Time(encounter_bs["Time Start"]).to_datetime()
+bs_encounter_end_time = Time(encounter_bs["Time End"]).to_datetime()
+
+mp_encounter_start_time = Time(encounter_mp["Time Start"]).to_datetime()
+mp_encounter_end_time = Time(encounter_mp["Time End"]).to_datetime()
+
+# Spatial indices of start and end of BS and MP encounters
+bs_start_idx = np.searchsorted(ephemeris_data["UTC"], bs_encounter_start_time, side="left")
+bs_end_idx = np.searchsorted(ephemeris_data["UTC"], bs_encounter_end_time, side="right")
+
+mp_start_idx = np.searchsorted(ephemeris_data["UTC"], mp_encounter_start_time, side="left")
+mp_end_idx = np.searchsorted(ephemeris_data["UTC"], mp_encounter_end_time, side="right")
+
+
+config = [
+    (bs_start_idx, bs_end_idx, "Bow Shock"),
+    (mp_start_idx, mp_end_idx, "Magnetopause"),
+]
+
+
+for start_idx, end_idx, title in config:
+
+    # Bin sizes, 0.01 Mercury Radii ^2 from -7 to 7 RM
+    xedges, yedges = np.arange(-7,7, 0.1),np.arange(-7,7, 0.1) 
+
+    # Size of bin matrix (minus one due to NumPy convention)
+    nx = len(xedges) - 1
+    ny = len(yedges) - 1
+
+    fig = plt.figure()
+
+    # Initialise non-standard sized axis for plotting
+    gs = GridSpec(2, 3, figure=fig)
+
+    ax = [
+            fig.add_subplot(gs[0, 0]),
+            fig.add_subplot(gs[0, 1]),
+            fig.add_subplot(gs[0, 2]), 
+            fig.add_subplot(gs[1, :]),
+            ]
+
+    # Config for each plane and cylindrical coords
+    encounter_per_hour_hist_configs = [
+            (ephemeris_data["X MSM"], ephemeris_data["Y MSM"], ax[0], "xy", "X MSM", "Y MSM"),
+            (ephemeris_data["X MSM"], ephemeris_data["Z MSM"], ax[1], "xz", "X MSM", "Z MSM"),
+            (ephemeris_data["Y MSM"], ephemeris_data["Z MSM"], ax[2], "yz", "Y MSM", "Z MSM"),
+            (ephemeris_data["X MSM"], ephemeris_rho, ax[3], "cylindrical", "X MSM", r"$\rho$ MSM"),
+            ]
+
+    for x, y, axis, label, xname, yname in encounter_per_hour_hist_configs:
+
+        # Change size for Cylindrical coords as they're not symmetric
+        if label == "cylindrical":
+            yedges = np.arange(0,10, 0.1)
+            ny = len(yedges) - 1
+
+        # Initialise bin matrix
+        encounter_count = np.zeros((nx,ny))
+
+        # Histogram of 'residence' of MESSENGER during entire mission
+        residence_hours, _, _= np.histogram2d(x.value, y.value, bins=[xedges, yedges], weights=residence_time)
+
+        hist_fig, hist_ax = plt.subplots()
+
+        # Plot residence histogram (transpose as first index (x) is a row not column)
+        hist_2d = hist_ax.pcolormesh(xedges, yedges, residence_hours.T, norm=LogNorm(), cmap='viridis')
+        hist_ax.set_xlabel(xname)
+        hist_ax.set_ylabel(yname)
+
+        hist_fig.colorbar(hist_2d, ax=hist_ax, label=r'MESSENGER Residence [hours]')
+
+        
+        # Find bins that encounters lie in
+
+        for i, (s, e) in enumerate(zip(start_idx, end_idx)):
+
+            # Consider encounters of single crossing
+            if s==e:
+                ix = np.digitize(x[s].value, xedges) - 1
+                iy = np.digitize(y[s].value, yedges) - 1
+
+            else:
+                ix = np.digitize(x[s:e].value, xedges) - 1
+                iy = np.digitize(y[s:e].value, yedges) - 1
+
+            # Pair indices as coordinates and remove duplicates (overcounting encounters per bin)
+            bin_pairs = np.column_stack([ix, iy])
+            unique_bins = np.unique(bin_pairs, axis=0)
+            
+            # Increase count of bin matrix for positions that encounter crossed through
+            encounter_count[unique_bins[:, 0], unique_bins[:, 1]] += 1
+
+
+        # Find rate of encounters per hour by dividing it (when possible i.e no NaN or 0) by how long MESSENGER was in each bin
+        with np.errstate(divide='ignore', invalid='ignore'):
+            rate_grid = np.where(residence_hours > 0, encounter_count / residence_hours, np.nan)
+
+
+        # Plot
+        pcm = axis.pcolormesh(xedges, yedges, rate_grid.T, norm=LogNorm(), cmap='viridis')
+
+        # Plot boundaries and label
+        if label == "cylindrical":
+            plot_magnetospheric_boundaries(axis, frame='MSM', sub_solar_magnetopause=1.4, cylindrical=True, add_legend=True, zorder=5)
+            axis.set_xlim(-8,6)
+            axis.set_ylim(0,10)
+            axis.legend()
+        else:
+            plot_magnetospheric_boundaries(axis, frame='MSM', sub_solar_magnetopause=1.4, plane=label, add_legend=False, zorder=5)
+            axis.set_xlim(-8,8)
+            axis.set_ylim(-8,8)
+
+        axis.set_xlabel(f"{xname} Mercury Radii")
+        axis.set_ylabel(f"{yname} Mercury Radii")
+
+
+        if label =="xy":
+            fig.colorbar(pcm, ax=ax, label= title + r' Encounters (hours)$^{-1}$ (Normalised by residence)')
+
+    fig.suptitle("Encounters per hour in MESSENGER Mission")
+
+plt.show()
